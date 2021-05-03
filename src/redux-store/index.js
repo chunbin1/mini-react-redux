e09c7b123574040577f0a4efb496eee31d69d176
licb <545360100@qq.com>
2021-05-03 16:43:31 +0800

    refactor: 💡 优化目录结构和例子

--- src/redux-store/index.js
+++ src/redux-store/index.js
@@ -1,12 +1,13 @@
-import { createStore, combineReducers } from "redux";
+import { createStore, combineReducers,applyMiddleware } from "redux";
 import { reducer, reducerTwo } from "../react-redux/reducer";
 import logger from "../react-redux/middleware";
 
 const combineReducer = combineReducers({
-    add: reducer,
-    multy: reducerTwo,
-  });
+  add: reducer,
+  multy: reducerTwo,
+});
+const store = createStore(combineReducer, applyMiddleware(logger)); //创建store
 
-const store = createStore(combineReducer,logger)
 store.dispatch({ type: "plus" }); //执行加法操作,给count加1
 
+export default store
--- src/store/example.js
+++ src/store/example.js
@@ -8,11 +8,6 @@ const combineReducer = combineReducers({
 });
 const store = createStore(combineReducer, applyMiddleware(logger)); //创建store
 
-console.log(store.getState());
-// store.subscribe(() => { console.log('组件1收到store的通知') })
-// store.subscribe(() => { console.log('组件2收到store的通知') })
 store.dispatch({ type: "plus" }); //执行加法操作,给count加1
-
-console.log(store.getState()); //获取state
 
 export default store
