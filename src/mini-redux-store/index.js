0d77c4a0bf80c451b1a5d1856f0cbbc6b0a0eaff
licb <545360100@qq.com>
2021-05-03 17:35:32 +0800

    feat: 🎸 例子调整

--- src/mini-redux-store/index.js
+++ src/mini-redux-store/index.js
@@ -2,8 +2,18 @@ import reducers from "../reducers/reducers-mini-redux";
 import logger from "../middleware/logger";
 import { createStore, applyMiddleware } from "../mini-redux";
 
-const store = createStore(reducers, applyMiddleware(logger)); //创建store
+const initState = {
+  colorReducer: { yellow: 100, name: "Mini的store" },
+  countReducer: {
+    count: 100,
+    name: "Mini的store",
+  },
+};
+const store = createStore(reducers, initState, applyMiddleware(logger)); //创建store
 
 store.dispatch({ type: "plus" }); //执行加法操作,给count加1
+store.dispatch({
+  type: "subtract",
+});
 
 export default store;
